5f9f810241b73802de1017f4efc69749f542e8a0
Lee Jeongyeon <leeeeeyeonn@gmail.com>
2021-07-27 16:11:18 +0900

    Edit CSS

--- src/routes/About.js
+++ src/routes/About.js
@@ -12,7 +12,6 @@ function About(){
                 <ul>
                 <li>CSS 큰 테두리는 바꾸지 말기, 나 CSS 잘 몰랑</li>
                 <li>Node.js API로 코드 수정</li>
-                <li>Themovie DB로 API 바꿔보기</li>
                 <li>회원가입, 로그인, 로그아웃, 회원정보 수정, 회원탈퇴</li>
                 <li>게시판</li>
                 <li>검색</li>
